eff03162a7c1ce27e08866beef67edd7155c52a0
MarcinPol89 <68320583+MarcinPol89@users.noreply.github.com>
2025-03-17 10:01:21 +0100

    Update upload_s3_python.py

--- upload_s3_python.py
+++ upload_s3_python.py
@@ -1,76 +1,77 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-"
+# -*- coding: utf-8 -*-
 
 import boto3
-from botocore.client import ClientError
+from botocore.exceptions import ClientError
 import requests
-import subprocess
 import os
 
-# Collecting information about EC2 instance from AWS service
+# Collecting information about EC2 instance from AWS metadata service
+USER_DATA_URL = 'http://169.254.169.254/latest/user-data'
+META_DATA_URL = 'http://169.254.169.254/latest/meta-data'
+EC2_INS_DATA_FILE = 'ec2InsDatafile'
 
-user_data = 'http://169.254.169.254/latest/user-data'
-meta_data = 'http://169.254.169.254/latest/meta-data'
-ec2InsDatafile = 'ec2InsDatafile'
+# Corrected dictionary with proper values
 ec2_params = {
-    'Instance ID': i-aaabbb,
-    'Reservation ID': 'reservation-id'
-    'Public IP': 8.8.8.8,
-    'Public Hostname': 'public_hostname',
-    'Private IP': 192.168.0.1'
-    'Security Groups': [one, two , three],
-    'AMI ID': 'ami_id'
+    'Instance ID': 'instance-id',
+    'Reservation ID': 'reservation-id',
+    'Public IP': 'public-ipv4',
+    'Public Hostname': 'public-hostname',
+    'Private IP': 'local-ipv4',
+    'Security Groups': 'security-groups',
+    'AMI ID': 'ami-id'
 }
 
 try:
-    fh = open(ec2InsDatafile, 'w')
-except:
-    print 'Error while opening file for write'
+    with open(EC2_INS_DATA_FILE, 'w') as fh:
+        for param, value in ec2_params.items():
+            try:
+                response = requests.get(f"{META_DATA_URL}/{value}")
+                response.raise_for_status()  # Raise an error for failed requests
+                data = response.text
+            except requests.RequestException as e:
+                print(f"Error while making request for {param}: {e}")
+                data = "N/A"
 
-for param, value in ec2_params.items():
-    try:
-        responce = requests.get(meta_data +'/' + value)
-    except:
-        print "Error while making request"
-    if isinstance(responce.text,list):
-        print responce.text +': is list'
-        data = ' '.joint(responce.text)
-    else:
-        data = param +":"+responce.text
-    try:
-          fh.write(data+'\r\n')
-    except:
-        print('Error during writing to file')
-        print data
+            # If response is a list, join elements with spaces
+            if isinstance(data, list):
+                print(f"{param}: is list")
+                data = ' '.join(data)
 
-#Getting  OS related if from system files
+            fh.write(f"{param}: {data}\n")
 
-os_name = os.popen("grep '^NAME' /etc/os-release | cut -d'=' -f2").read().strip()
-os_version = os.popen("grep '^VERSION=' /etc/os-release | cut -d'=' -f2").read().strip()
-os_users = os.popen("grep -E 'bash|sh' /etc/passwd | awk -F: '{print $1}' | xargs echo").read().strip()
+        # Getting OS-related info from system files
+        os_name = os.popen("grep '^NAME' /etc/os-release | cut -d'=' -f2").read().strip()
+        os_version = os.popen("grep '^VERSION=' /etc/os-release | cut -d'=' -f2").read().strip()
+        os_users = os.popen("grep -E 'bash|sh' /etc/passwd | awk -F: '{print $1}' | xargs echo").read().strip()
 
-try:
-    fh.write(os_name_val+'\r\n')
-    fh.write(os_vers_val+'\r\n')
-    fh.write(os_usrs_val+'\r\n')
-except:
-    print "Error during write to file"
-    fh.close()
+        fh.write(f"OS NAME: {os_name}\n")
+        fh.write(f"OS VERSION: {os_version}\n")
+        fh.write(f"Login-able users: {os_users}\n")
 
+except IOError as e:
+    print(f"Error while writing to file: {e}")
 
-# Upload file to s3 storage
-s3_bucket_name = 'new-bucket-e05ab0e0'
+# Upload file to S3 storage
+S3_BUCKET_NAME = 'new-bucket-e05ab0e0'
 s3_conn = boto3.client('s3')
 
 try:
-    s3.meta.client.head_bucket(Bucket=s3_bucket_name)
+    # Check if the bucket exists
+    s3_conn.head_bucket(Bucket=S3_BUCKET_NAME)
 
-    with (ec2InsDatafile, 'r') as fh:
+    # Get instance ID
+    instance_id = requests.get(f"{META_DATA_URL}/instance-id").text.strip()
+
+    # Upload file
+    with open(EC2_INS_DATA_FILE, 'r') as fh:
         s3_conn.put_object(
-            Bucket=s3_bucket_name,
-            Key='system_info' + requests.get(meta_data +'/' + instance_id) + '.txt',
+            Bucket=S3_BUCKET_NAME,
+            Key=f"system_info_{instance_id}.txt",
             Body=fh.read()
         )
-    print("File has been uploaded into " + s3_bucket_name + " S3 bucket with instance_id key.")
-except ClientError:
-    "Are you sure the destination bucket exist? Check it."
+
+    print(f"File has been uploaded to {S3_BUCKET_NAME} S3 bucket with key: system_info_{instance_id}.txt")
+
+except ClientError as e:
+    print(f"Are you sure the destination bucket exists? Check it. Error: {e}")
